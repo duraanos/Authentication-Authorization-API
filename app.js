8e91677ea9fc0b3e7de8b15fabd9bb50681eec15
duraanos <duran364@anadolu.edu.tr>
2025-09-04 19:37:59 +0300

    refactor(app): Migrate to ESM and integrate auth routes

--- app.js
+++ app.js
@@ -1,18 +1,14 @@
-const express = require('express');
-require('dotenv').config();
+import express from 'express';
+import authRoutes from './auth/routes/auth.routes.js';
+import dotenv from 'dotenv';
+dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 
-app.get('/api/health', (req, res) => {
-  res.status(200).json({
-    status: 'OK',
-    message: 'Server is running',
-    timestamp: new Date().toISOString(),
-  });
-});
+app.use('/api/auth', authRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
