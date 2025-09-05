7eb43a8ee44e9ce4dae5bddcfb775cf1468cd5e7
duraanos <duran364@anadolu.edu.tr>
2025-09-05 11:36:57 +0300

    feat(auth): Add currentUser routes.js

--- auth/routes/auth.routes.js
+++ auth/routes/auth.routes.js
@@ -1,14 +1,21 @@
 import express from 'express';
-import { register, login, logout } from '../controller/auth.controller.js';
+import {
+  register,
+  login,
+  logout,
+  currentUser,
+} from '../controller/auth.controller.js';
 import {
   validateRegister,
   validateLogin,
 } from '../validators/auth.validator.js';
+import { verifySupabaseToken } from '../middlewares/authenticationToken.js';
 
 const router = express.Router();
 
 router.post('/register', validateRegister, register);
 router.post('/login', validateLogin, login);
 router.post('/logout', logout);
+router.get('/current', verifySupabaseToken, currentUser);
 
 export default router;
