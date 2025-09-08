d2bc27c392b74bca30cf0c27459f59d8d7a80b57
duraanos <duran364@anadolu.edu.tr>
2025-09-08 16:31:40 +0300

    feat(auth): Add resetPasswordSchema for password reset validation

--- auth/validators/auth.validator.js
+++ auth/validators/auth.validator.js
@@ -52,3 +52,28 @@ export const validateLogin = (req, res, next) => {
   if (error) return res.status(400).json({ error: error.details[0].message });
   next();
 };
+
+export const resetPasswordSchema = Joi.object({
+  access_token: Joi.string().required().messages({
+    'string.empty': 'Access token is required',
+    'any.required': 'Access token is required',
+  }),
+  newPassword: Joi.string()
+    .min(8)
+    .pattern(new RegExp('^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])'))
+    .required()
+    .messages({
+      'string.empty': 'Password is required',
+      'string.min': 'Password must be at least 8 characters long',
+      'string.pattern.base':
+        'Password must contain at least one uppercase letter, one lowercase letter, and one number',
+      'any.required': 'Password is required',
+    }),
+});
+
+export const validateResetPassword = (req, res, next) => {
+  const { error } = registerSchema.validate(req.body);
+
+  if (error) return res.status(400).json({ error: error.details[0].message });
+  next();
+};
