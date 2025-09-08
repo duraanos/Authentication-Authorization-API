3c9b85ec60e9d097762def5251c0c5984c2edfc8
duraanos <duran364@anadolu.edu.tr>
2025-09-08 16:08:36 +0300

    feat(app): Register admin routes and enable urlencoded middleware

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
 import express from 'express';
 import authRoutes from './auth/routes/auth.routes.js';
+import adminRoutes from './auth/routes/admin.routes.js';
 import dotenv from 'dotenv';
+
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use('/api/auth', authRoutes);
+app.use('/api/admin', adminRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
